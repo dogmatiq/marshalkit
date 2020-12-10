dc7b6627f6edea8372d48bedba0a4d55486d5be7
Danil Petrov <danilpetrovvi@gmail.com>
2020-12-10 16:29:24 +1000

    Move in message marshaling funcs.

---
+++ envelope.go
@@ -0,0 +1,77 @@
+package marshalkit
+
+import (
+	"time"
+
+	"github.com/dogmatiq/configkit"
+	"github.com/dogmatiq/dogma"
+	"github.com/dogmatiq/envelopespec"
+)
+
+// MustMarshalMessageIntoEnvelope marshals a Dogma message into an envelope.
+func MustMarshalMessageIntoEnvelope(
+	vm ValueMarshaler,
+	m dogma.Message,
+	env *envelopespec.Envelope,
+) {
+	p := MustMarshal(vm, m)
+
+	_, n, err := p.ParseMediaType()
+	if err != nil {
+		// CODE COVERAGE: This branch would require the marshaler to violate its
+		// own requirements on the format of the media-type.
+		panic(err)
+	}
+
+	env.PortableName = n
+	env.MediaType = p.MediaType
+	env.Data = p.Data
+}
+
+// UnmarshalMessageFromEnvelope unmarshals a Dogma message from an envelope.
+func UnmarshalMessageFromEnvelope(
+	vm ValueMarshaler,
+	env *envelopespec.Envelope,
+) (dogma.Message, error) {
+	p := Packet{
+		MediaType: env.MediaType,
+		Data:      env.Data,
+	}
+
+	return vm.Unmarshal(p)
+}
+
+// MustMarshalEnvelopeIdentity marshals id to its protocol buffers
+// representation, as used within message envelopes.
+func MustMarshalEnvelopeIdentity(id configkit.Identity) *envelopespec.Identity {
+	return &envelopespec.Identity{
+		Name: id.Name,
+		Key:  id.Key,
+	}
+}
+
+// UnmarshalEnvelopeIdentity unmarshals id from its protocol buffers
+// representation, as used within message envelopes.
+func UnmarshalEnvelopeIdentity(id *envelopespec.Identity) (configkit.Identity, error) {
+	return configkit.NewIdentity(id.Name, id.Key)
+}
+
+// MustMarshalEnvelopeTime marshals t to its RFC-3339 representation, as used
+// within envelopes.
+func MustMarshalEnvelopeTime(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.Format(time.RFC3339Nano)
+}
+
+// UnmarshalEnvelopeTime unmarshals a t from its RFC-3339 representation, as
+// used within envelopes.
+func UnmarshalEnvelopeTime(t string) (time.Time, error) {
+	if len(t) == 0 {
+		return time.Time{}, nil
+	}
+
+	return time.Parse(time.RFC3339Nano, t)
+}
---
+++ envelope_test.go
@@ -0,0 +1,111 @@
+package marshalkit_test
+
+import (
+	"time"
+
+	"github.com/dogmatiq/configkit"
+	. "github.com/dogmatiq/dogma/fixtures"
+	. "github.com/dogmatiq/envelopespec"
+	. "github.com/dogmatiq/marshalkit"
+	"github.com/dogmatiq/marshalkit/fixtures"
+	. "github.com/jmalloc/gomegax"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("func MarshalMessage()", func() {
+	It("marshals the message into the envelope", func() {
+		var env Envelope
+
+		MustMarshalMessageIntoEnvelope(
+			fixtures.Marshaler,
+			MessageA1,
+			&env,
+		)
+
+		Expect(&env).To(EqualX(
+			&Envelope{
+				PortableName: fixtures.MessageAPortableName,
+				MediaType:    fixtures.MessageA1Packet.MediaType,
+				Data:         fixtures.MessageA1Packet.Data,
+			},
+		))
+	})
+})
+
+var _ = Describe("func UnmarshalMessage()", func() {
+	It("unmarshals the message from the envelope", func() {
+		env := &Envelope{
+			PortableName: fixtures.MessageAPortableName,
+			MediaType:    fixtures.MessageA1Packet.MediaType,
+			Data:         fixtures.MessageA1Packet.Data,
+		}
+
+		m, err := UnmarshalMessageFromEnvelope(fixtures.Marshaler, env)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(m).To(Equal(MessageA1))
+	})
+})
+
+var _ = Describe("func MarshalIdentity()", func() {
+	It("returns the protocol buffers identity", func() {
+		in := configkit.MustNewIdentity("<name>", "<key>")
+
+		out := MustMarshalEnvelopeIdentity(in)
+		Expect(out).To(EqualX(
+			&Identity{
+				Name: "<name>",
+				Key:  "<key>",
+			},
+		))
+	})
+})
+
+var _ = Describe("func UnmarshalIdentity()", func() {
+	It("returns the configkit identity", func() {
+		in := &Identity{
+			Name: "<name>",
+			Key:  "<key>",
+		}
+
+		out, err := UnmarshalEnvelopeIdentity(in)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(out).To(Equal(
+			configkit.MustNewIdentity("<name>", "<key>"),
+		))
+	})
+})
+
+var _ = Describe("func MarshalTime()", func() {
+	It("returns the time formatted as per RFC-3339", func() {
+		in := time.Date(2001, 02, 03, 04, 05, 06, 0, time.UTC)
+
+		out := MustMarshalEnvelopeTime(in)
+		Expect(out).To(Equal("2001-02-03T04:05:06Z"))
+	})
+
+	It("returns an empty string if the time is the zero-value", func() {
+		var in time.Time
+
+		out := MustMarshalEnvelopeTime(in)
+		Expect(out).To(Equal(""))
+	})
+})
+
+var _ = Describe("func UnmarshalTime()", func() {
+	It("parses the time from RFC-3339 format", func() {
+		in := "2001-02-03T04:05:06Z"
+
+		out, err := UnmarshalEnvelopeTime(in)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		expect := time.Date(2001, 02, 03, 04, 05, 06, 0, time.UTC)
+		Expect(out).To(BeTemporally("==", expect))
+	})
+
+	It("returns the zero-value if the input is empty", func() {
+		out, err := UnmarshalEnvelopeTime("")
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(out.IsZero()).To(BeTrue())
+	})
+})
