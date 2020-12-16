6d500d70f6f00ca83ec094fdd030300d2071cf47
Danil Petrov <danilpetrovvi@gmail.com>
2020-12-16 15:00:12 +1000

    Arrange MIME helpers into an internal pkg.

--- internal/mimex/mime.go
+++ internal/mimex/mime.go
@@ -1,19 +1,33 @@
 package mimex
 
+import (
+	"fmt"
+	"mime"
+)
+
+// FormatMediaType returns the media-type as the base media type and the
+// message's portable name as the 'type' parameter.
+func FormatMediaType(base string, portableName string) string {
+	return mime.FormatMediaType(
+		base,
+		map[string]string{"type": portableName},
+	)
+}
+
 // ParseMediaType returns the media-type and the portable type name encoded in
 // the packet's MIME media-type.
-// func ParseMediaType(mt string) (string, string, error) {
-// 	mt, params, err := mime.ParseMediaType(p.MediaType)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
+func ParseMediaType(mediatype string) (string, string, error) {
+	mt, params, err := mime.ParseMediaType(mediatype)
+	if err != nil {
+		return "", "", err
+	}
 
-// 	if n, ok := params["type"]; ok {
-// 		return mt, n, nil
-// 	}
+	if n, ok := params["type"]; ok {
+		return mt, n, nil
+	}
 
-// 	return "", "", fmt.Errorf(
-// 		"the media-type '%s' does not specify a 'type' parameter",
-// 		p.MediaType,
-// 	)
-// }
+	return "", "", fmt.Errorf(
+		"the media-type '%s' does not specify a 'type' parameter",
+		mediatype,
+	)
+}
