eab0fad0cfdd436c7f056fb863db7dd2a3cb4be6
James Harris <contact@jamesharris.id.au>
2024-09-29 07:40:01 +1000

    Mark marshalkit as deprecated.

--- doc.go
+++ doc.go
@@ -1,3 +1,5 @@
 // Package marshalkit provides utilities for marshaling and unmarshaling
 // Dogma messages and other types.
+//
+// Deprecated: Use [github.com/dogmatiq/enginekit/marshaler] instead.
 package marshalkit
